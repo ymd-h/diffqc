ca1ae95795442e0ec578674f3b70c68eed37b304
Yamada <ymd_h@outlook.jp>
2023-01-02 22:32:49 +0900

    Add test for to_state

--- test/test_dense.py
+++ test/test_dense.py
@@ -11,6 +11,15 @@ class TestZero(unittest.TestCase):
         z = dense.zero(2, jnp.complex64)
         np.testing.assert_allclose(z.shape, (2, 2))
         self.assertEqual(z[0, 0], 1+0j)
+        self.assertEqual(jnp.sum(z), 1+0j)
+
+class TestToState(unittest.TestCase):
+    def test_to_state(self):
+        z = dense.zero(3, jnp.complex64)
+        s = dense.to_state(z)
+        np.testing.assert_allclose(s.shape, (8,))
+        self.assertEqual(s[0], 1+0j)
+        self.assertEqual(jnp.sum(s), 1+0j)
 
 
 if __name__ == "__main__":
