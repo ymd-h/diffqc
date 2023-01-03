b5ae0d73d2c6ea5a429b4ad577e5ac9506031ce2
Yamada <ymd_h@outlook.jp>
2023-01-03 21:08:17 +0900

    Add sample

--- diffq/util.py
+++ diffq/util.py
@@ -4,6 +4,7 @@ import jax.numpy as jnp
 __all__ = [
     "prob",
     "marginal_prob",
+    "sample",
 ]
 
 
@@ -11,6 +12,13 @@ def prob(state):
     return jnp.square(jnp.abs(state))
 
 
-def marginal_prob(prob, integrage_wires):
-    p = jnp.reshape(prob, (2,) * int(jnp.log2(prob.shape[0])))
+def marginal_prob(probs, integrage_wires):
+    p = jnp.reshape(probs, (2,) * int(jnp.log2(probs.shape[0])))
     return jnp.sum(p, axis=integrage_wires)
+
+
+def sample(key, probs, shape):
+    return jax.random.choice(key,
+                             jnp.arange(probs.shape[0]),
+                             shape=shape,
+                             p=probs)
