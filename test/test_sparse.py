bfbaca2b322d1c782f69f9a08b6ab4c0c4996be5
Yamada <ymd_h@outlook.jp>
2023-01-04 23:59:40 +0900

    Test for Hadamard

--- test/test_sparse.py
+++ test/test_sparse.py
@@ -21,6 +21,24 @@ class TestToState(unittest.TestCase):
         self.assertEqual(s[0], 1+0j)
         self.assertEqual(jnp.sum(s), 1+0j)
 
+class TestHadamard(unittest.TestCase):
+    def test_0(self):
+        w = (0,)
+        z = sparse.zeros(1, jnp.complex64)
+        h = sparse.Hadamard(z, w)
+        np.testing.assert_allclose(h[0, 0], [1/jnp.sqrt(2), 1/jnp.sqrt(2)])
+
+    def test_1(self):
+        w = (0,)
+        z = sparse.zeros(1, jnp.complex64).at[:,:].set([0, 1])
+        h = sparse.Hadamard(z, w)
+        np.testing.assert_allclose(h[0, 0], [1/jnp.sqrt(2), -1/jnp.sqrt(2)])
+
+    def test_twice(self):
+        w = (0,)
+        z = sparse.zeros(1, jnp.complex64)
+        zz = sparse.Hadamard(sparse.Hadamard(z, w), w)
+        np.testing.assert_allclose(zz, z)
 
 if __name__ == "__main__":
     unittest.main()
