3cb71079a2a34076146de46a7a2d57ee4aa0c54e
Yamada <ymd_h@outlook.jp>
2023-01-04 10:42:07 +0900

    Add comments at example

--- example/00-circuit-basics.py
+++ example/00-circuit-basics.py
@@ -10,26 +10,45 @@ def main():
 
     @jax.jit
     def circuit(params):
+        # Initialize |00..0> state
         x = op.zeros(nqubits, jnp.complex64)
 
+        # Apply quantum operation
+        # At quantum operator,
+        # - the first argument is qubit state,
+        # - the second argument is wire(s) and its type is `tuple`,
+        # - the rest arguments are parameters, and
+        # - the return value is the new qubit state.
+
         for i in range(nqubits):
+            # 1 qubit operation H on i-th wire
             x = op.Hadamard(x, (i,))
 
         for i in range(nqubits-1):
+            # 2 qubit operation CRZ on i-th and (i+1)-th wires
+            # The first wire is the control qubit.
             x = op.CRZ(x, (i, i+1), params["CRZ"][i])
 
         x = op.Rot(x, (4, ),
                    params["Rot"]["phi"], params["Rot"]["theta"], params["Rot"]["omega"])
 
+        # Convert internal representation to state-vector
+        # aka. [|00000>, |00001>, |00010>, ..., |11111>]
         return op.to_state(x)
 
     @jax.jit
     def expval(params):
         s = circuit(params)
+
+        # Caluculate probabilities of each state
         p = diffq.prob(s)
+
+        # Expectation of |1> at certain wire.
         return diffq.expval(p, 4)
 
 
+    # Define parameters as compatible with JAX pytree.
+    # Ref: https://jax.readthedocs.io/en/latest/pytrees.html
     p = {
         "CRZ": jnp.ones((4,)),
         "Rot": {
@@ -39,6 +58,7 @@ def main():
         }
     }
 
+    # Now you can call circuit and take gradient.
     print(f"expval: {expval(p)}")
     print(f"grad: {jax.grad(expval)(p)}")
 
