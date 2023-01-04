272de8bc27df36fab64d06503767208f8794cd3e
Yamada <ymd_h@outlook.jp>
2023-01-04 23:39:48 +0900

    Test to_state

--- test/test_sparse.py
+++ test/test_sparse.py
@@ -13,6 +13,14 @@ class TestZeros(unittest.TestCase):
         np.testing.assert_allclose(z[0, 0], [1, 0])
         np.testing.assert_allclose(z[0, 1], [1, 0])
 
+class TestToState(unittest.TestCase):
+    def test_to_state(self):
+        z = sparse.zeros(3, jnp.complex64)
+        s = sparse.to_state(z)
+        np.testing.assert_allclose(s.shape, (8,))
+        self.assertEqual(s[0], 1+0j)
+        self.assertEqual(jnp.sum(s), 1+0j)
+
 
 if __name__ == "__main__":
     unittest.main()
