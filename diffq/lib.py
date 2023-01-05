d82d65197cb66f3cecf4fc10c01a3d21b8fd8129
Yamada <ymd_h@outlook.jp>
2023-01-05 23:22:28 +0900

    Add lib.GHZ()

---
+++ diffq/lib.py
@@ -0,0 +1,33 @@
+from typing import Tuple
+
+import jax
+import jax.numpy as jnp
+
+
+def GHZ(op, c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Create Greenberger–Horne–Zeilinger state from |00...0> state
+
+    |00...0> -> (|00...0> + |11...1>)/sqrt(2)
+
+    Parameters
+    ----------
+    op
+        `dense` or `sparse` module
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
+    assert len(wires) >= 2, f"BUG: GHZ needs len(wires) >= 2, got {len(wires)}."
+
+    c = op.Hadamard(c, (wires[0],))
+    for i in range(len(wires)-1):
+        c = op.CNOT(c, (wires[i], wires[i+1]))
+
+    return c
