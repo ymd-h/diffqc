b36827ccedd82796faa8fa1fa9f3bf326292c3ec
Yamada <ymd_h@outlook.jp>
2023-01-08 16:17:31 +0900

    Docstring for sparse

--- diffq/sparse.py
+++ diffq/sparse.py
@@ -55,10 +55,38 @@ __all__ = [
 ]
 
 # StateVec Shape: [possibility, nqubit, qubit]
-def zeros(nqubits, dtype):
+def zeros(nqubits: int, dtype: jnp.dtype) -> jnp.ndarray:
+    """
+    Create |00...0> state
+
+    Parameters
+    ----------
+    nqubits : int
+        Number of qubits
+    dtype : jnp.dtype
+        dtype.
+
+    Returns
+    -------
+    jnp.ndarray
+        Zero state aka. |00...0>
+    """
     return jnp.zeros((1, nqubits, 2), dtype=dtype).at[:,:,0].set(1)
 
-def to_state(x):
+def to_state(x: jnp.ndarray) -> jnp.ndarray:
+    """
+    Convert qubits internal representation to state vector
+
+    Parameters
+    ----------
+    x : jnp.ndarray
+        qubits state
+
+    Returns
+    -------
+    jnp.ndarray
+        State vector, e.g. [|000>, |001>, |010>, ..., |111>]
+    """
     if x.shape[0] > (2 ** x.shape[1]):
         jax.debug.print(
             "Warning: This circuit used required more memory than `dense` does. " +
@@ -140,51 +168,276 @@ def entangle_op2(op):
 
 
 # Quantum Operators
-def Hadamard(c, wires):
+def Hadamard(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply Hadamard Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.H(c.dtype))
 
-def PauliX(c, wires):
+def PauliX(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply Pauli X Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.sigmaX(c.dtype))
 
-def PauliY(c, wires):
+def PauliY(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply Pauli Y Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.sigmaY(c.dtype))
 
-def PauliZ(c, wires):
+def PauliZ(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply Pauli Z Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.sigmaZ(c.dtype))
 
-def S(c, wires):
+def S(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply S Gate (Single qubite Phase Gate)
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.phaseS(c.dtype))
 
-def T(c, wires):
+def T(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply T Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.phaseT(c.dtype))
 
-def SX(c, wires):
+def SX(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply Square Root X Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.sqrtX(c.dtype))
 
-def CNOT(c, wires):
+def CNOT(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply controlled-NOT Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, control_op(_op.sigmaX(c.dtype)))
 
-def CZ(c, wires):
+def CZ(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply controlled-Z Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, control_op(_op.sigmaZ(c.dtype)))
 
-def CY(c, wires):
+def CY(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply controlled-Y Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, control_op(_op.sigmaY(c.dtype)))
 
-def SWAP(c, wires):
+def SWAP(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply SWAP Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, lambda q12: jnp.expand_dims(q12.at[(1, 0), :].get(), 0))
 
-def ISWAP(c, wires):
+def ISWAP(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply i-SWAP Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, entangle_op2(_op.ISWAP(c.dtype)))
 
-def ECR(c, wires):
+def ECR(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply Echoed RZX(pi/2) Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, entangle_op2(_op.ECR(c.dtype)))
 
-def SISWAP(c, wires):
+def SISWAP(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply Square Root i-SWAP Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, entangle_op2(_op.SISWAP(c.dtype)))
 
 SQISWAP = SISWAP
 
-def CSWAP(c, wires):
+def CSWAP(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply controlled SWAP Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``3``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     def cswap(q123):
         s0 = q123.at[0, 0].get()
         s1 = q123.at[0, 1].get()
@@ -195,7 +448,22 @@ def CSWAP(c, wires):
                 .at[1, 1:3, :].set(s1 * q123.at[(2,1), :].get()))
     return opN(c, wires, cswap)
 
-def Toffoli(c, wires):
+def Toffoli(c: jnp.ndarray, wires: Tuple[int]) -> jnp.ndarray:
+    """
+    Apply Toffoli Gate (controlled controlled X Gate)
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``3``.
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     def toffoli(q123):
         s0 = q123.at[0, 0].get()
         s10 = q123.at[0, 1].get() * q123.at[1, 0].get()
@@ -211,56 +479,334 @@ def Toffoli(c, wires):
     return opN(c, wires, toffoli)
 
 
-def Rot(c, wires, phi, theta, omega):
+def Rot(c: jnp.ndarray, wires: Tuple[int],
+        phi: float, theta: float, omega: float) -> jnp.ndarray:
+    """
+    Apply Rotation Gate
+
+    Rot(phi, theta, omega) = RZ(omega)RY(theta)RZ(phi)
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+    phi, theta, omega : float
+        rotation angles
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.Rot(c.dtype, phi, theta, omega))
 
-def RX(c, wires, phi):
+def RX(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply Rotation X Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.RX(c.dtype, phi))
 
-def RY(c, wires, phi):
+def RY(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply Rotation Y Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.RY(c.dtype, phi))
 
-def RZ(c, wires, phi):
+def RZ(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply Rotation Z Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.RZ(c.dtype, phi))
 
-def PhaseShift(c, wires, phi):
+def PhaseShift(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply Local Phase Shift Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.phaseShift(c.dtype, phi))
 
-def ControlledPhaseShift(c, wires, phi):
+def ControlledPhaseShift(c: jnp.ndarray, wires: Tuple[int],
+                         phi: float) -> jnp.ndarray:
+    """
+    Apply controlled Phase Shift Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, control_op(_op.phaseShift(c.dtype, phi)))
 
 CPhase = ControlledPhaseShift
 
-def CPhaseShift00(c, wires, phi):
+def CPhaseShift00(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply Phase Shift Gate for |00>
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, entangle_op2(_op.phaseShift00(c.dtype, phi)))
 
-def CPhaseShift01(c, wires, phi):
+def CPhaseShift01(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply Phase Shift Gate for |01>
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, entangle_op2(_op.phaseShift01(c.dtype, phi)))
 
-def CPhaseShift10(c, wires, phi):
+def CPhaseShift10(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply Phase Shift Gate for |10>
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, entangle_op2(_op.phaseShift10(c.dtype, phi)))
 
-def CRX(c, wires, phi):
+def CRX(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply controlled RX Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, control_op(_op.RX(c.dtype, phi)))
 
-def CRY(c, wires, phi):
+def CRY(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply controlled RY Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, control_op(_op.RY(c.dtype, phi)))
 
-def CRZ(c, wires, phi):
+def CRZ(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply controlled RZ Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, control_op(_op.RZ(c.dtype, phi)))
 
-def CRot(c, wires, phi, theta, omega):
+def CRot(c: jnp.ndarray, wires: Tuple[int],
+         phi: float, theta: float, omega: float) -> jnp.ndarray:
+    """
+    Apply controlled Rotation Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi, theta, omega : float
+        rotation angles
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, control_op(_op.Rot(c.dtype, phi, theta, omega)))
 
 U1 = PhaseShift
 
-def U2(c, wires, phi, delta):
+def U2(c: jnp.ndarray, wires: Tuple[int], phi: float, delta: float) -> jnp.ndarray:
+    """
+    Apply controlled U2 Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+    phi, delta : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.U2(c.dtype, phi, delta))
 
-def U3(c, wires, theta, phi, delta):
+def U3(c: jnp.ndarray, wires: Tuple[int],
+       theta: float, phi: float, delta: float) -> jnp.ndarray:
+    """
+    Apply U3 Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``1``.
+    theta, phi, delta : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return op1(c, wires, _op.U3(c.dtype, theta, phi, delta))
 
-def PSWAP(c, wires, phi):
+def PSWAP(c: jnp.ndarray, wires: Tuple[int], phi: float) -> jnp.ndarray:
+    """
+    Apply Phase SWAP Gate
+
+    Parameters
+    ----------
+    c : jnp.ndarray
+        qubits state
+    wires : tuple of ints
+        wire to apply. ``len(wires)`` must be ``2``.
+    phi : float
+        rotation angle
+
+    Returns
+    -------
+    jnp.ndarray
+        applied qubits state
+    """
     return opN(c, wires, entangle_op2(_op.PSWAP(c.dtype, phi)))
 
 
