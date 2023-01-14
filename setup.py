7ddf7a402dc45b5168fc54968f805d5c9873eee6
Yamada <ymd_h@outlook.jp>
2023-01-14 16:37:38 +0900

    Add classifier

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(name="diffq",
       description="Diiferentiable Quantum Simulator",
       version="0.0.0",
       packages=find_packages(),
+      classifiers = [
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Topic :: Scientific/Engineering",
+      ],
       install_requires=["jax", "jaxlib"],
       extras_require = {
           "pennylane": ["pennylane"],
