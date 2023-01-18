931a91c6ae7ff185c7b5e22b00c82db65e9f4b39
Yamada <ymd_h@outlook.jp>
2023-01-18 20:16:24 +0900

    Josephson Sampler

--- diffqc/nn.py
+++ diffqc/nn.py
@@ -55,6 +55,80 @@ def CircuitCentricBlock(op, c: jnp.ndarray, wires: Tuple[int],
     return c
 
 
+def JosephsonSampler(op, c: jnp.ndarray, wires: Tuple[int],
+                      weights: jnp.ndarray) -> jnp.ndarray:
+    """
+    Apply Josephson Sampler as Parameterized Quantum Circuit (PQC)
+
+    Parameters
+    ----------
+    op
+        `dense` or `sparse`
+    c : jnp.ndarray
+        qubits
+    wires : tuple of ints
+        wires. Ususally, `(0, ..., qubits-1)`
+    weights : jnp.ndarray
+        parameters of rotation with shape of `(layers, )`
+
+    Returns
+    -------
+    jnp.ndarray
+        applied circuit
+
+    Notes
+    -----
+    Josephson Sampler circuit described at [1]_.
+    According to [2]_, for middle scale circuit (4, 6, and 8 qubits)
+    three layers have enough expressivity.
+
+    References
+    ----------
+    .. [1] M. R. Geller, "Sampling and scrambling on a chain of superconducting
+       qubits", Phys. Rev. Applied 10, 024052 (2018) (arXiv:1711.11026)
+    .. [2] S. Sim /et al/., "Expressibility and entangling capability of
+       parameterized quantum circuits for hybrid quantum-classical algorithms",
+       Adv. Quantum Technol. 2 (2019) 1900070 (arXiv:1905.10876)
+    """
+    assert (weights.ndim == 2) and (weights.shape[1] == 4 * (len(wires) - 1))
+
+    # qubits
+    #   even => n1 = qubits
+    #           n2 = qubits - 2
+    #   odd  => n1 = qubits - 1
+    #           n2 = qubits - 1
+    #
+    # => n1 + n2 = 2 * (qubits - 1)
+    n  = len(wires)
+    n1 = n - (n % 2)
+
+    # RY: [   0   ,   n1     )
+    # RZ: [  n1   , 2*n1     )
+    # RY: [2*n1   , 2*n1+  n2)
+    # RZ: [2*n1+n2, 2*n1+2*n2)
+    def Layer(ci, w):
+        for i in range(0, n-1, 2):
+            ci = op.RY(ci, (wires[i],), w.at[i   ].get())
+            ci = op.RZ(ci, (wires[i],), w.at[i+n1].get())
+
+            ci = op.RY(ci, (wires[i+1],), w.at[i+1   ].get())
+            ci = op.RZ(ci, (wires[i+1],), w.at[i+1+n1].get())
+
+            ci = op.CX(ci, (wires[i], wires[i+1]))
+
+        for i in range(1, n-1, 2):
+            ci = op.RY(ci, (wires[i],), w.at[i+2*n1   ].get())
+            ci = op.RZ(ci, (wires[i],), w.at[i+2*n1+n2].get())
+
+            ci = op.RY(ci, (wires[i+1],), w.at[i+1+2*n1   ].get())
+            ci = op.RZ(ci, (wires[i+1],), w.at[i+1+2*n1+n2].get())
+
+            ci = op.CX(ci, (wires[i], wires[i+1]))
+
+        return ci, None
+
+    c, _ = jax.lax.scan(Layer, c, weights)
+    return c
 
 
 def Convolution(op,
